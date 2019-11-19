ed5b7e5c7c407eae98c911e6109d66a69f949e9f
Justin Mittereder <jmittere@umw.edu>
2019-11-19 03:06:29 +0000

    added the Book class, so the Book can be used to read a message

---
+++ Book.java
@@ -0,0 +1,21 @@
+
+public class Book extends Item{
+
+	//usable book class that will generate books that the "use" method will override
+	
+	private ItemType type;
+	private String message;
+	
+	public Book(ItemType itemType, String name, int weight, int value, int strength, String message){
+		super(ItemType.OTHER, name, weight, value, strength);
+		this.message = message;
+	}
+
+	public void use(Player player){
+		System.out.println(this.message);
+	}
+
+
+
+
+}
