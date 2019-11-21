6047267c4cb3d8a4cbf52b3ebb6aab3a2df0b909
sburkley <sburkley@mail.umw.edu>
2019-11-21 15:06:24 +0000

    monsters aren't stupid now

--- Monster.java
+++ Monster.java
@@ -8,6 +8,7 @@ public class Monster extends Character{
 	private int random1 = 12;
 	private int random2 = 12;
 	private char character;
+	private int numMoves = 0;
 	
 	/**This is the Constructor for a Monster**/
 	public Monster(String name, int health, int strength){
@@ -35,53 +36,70 @@ public class Monster extends Character{
 	public char[][] moveMonster(char[][] square){//moves the monster randomly around the map
 		Random rand = new Random();
 		
-	/*	for(int x = 0; x < 10; x++){
-			for(int y = 0; y < 10; y++){
-				if(square[x][y] == this.character){
-					System.out.println("AAAA");
-					square[x][y] = ' ';
-				}
-			}
-		}*/
 		if(random1 == 12 || random2 == 12){
 			for(int i = 0; i < 4; i++){//this is to make 3 monsters
 				random1 = rand.nextInt(8)+1;
 				random2 = rand.nextInt(8)+1;
 				if(square[random1][random2] == '_' || square[random1][random2] == '|'){
-					//System.out.println("BBBBBB");
 					square[1][1] = this.character;
 				}
 				else{
 					square[random1][random2] = this.character;
-					//System.out.println("CCCCCC");
 				}
 			}
 		}//end if
 		else{
-			if(square[random1+1][random2] != '_' && square[random1+1][random2] != '|'){
+			//System.out.println("Monster has moved: " + numMoves);
+			if(numMoves == 9){
+				numMoves = 0;
+			}
+			if(numMoves <= 4){
+				if(square[random1+1][random2] != '_' && square[random1+1][random2] != '|'){
 					square[random1+1][random2] = this.character;
 					random1 = random1+1;
-					//System.out.println("MOVE");
+					
 				}
-			else if(square[random1-1][random2] != '_' && square[random1-1][random2] != '|'){
+				else if(square[random1-1][random2] != '_' && square[random1-1][random2] != '|'){
 					square[random1-1][random2] = '!';
 					random1 = random1 - 1;
-					//System.out.println("MOVE");
 
 				}
-			else if(square[random1][random2+1] != '_' && square[random1][random2+1] != '|'){
+				else if(square[random1][random2+1] != '_' && square[random1][random2+1] != '|'){
 					square[random1][random2+1] = this.character;
 					random2 = random2+1;
-					//System.out.println("MOVE");
 
 				}
-			else if(square[random1][random2-1] != '_' && square[random1][random2-1] != '|'){
+				else if(square[random1][random2-1] != '_' && square[random1][random2-1] != '|'){
 					square[random1][random2-1] = this.character;
 					random2 = random2-1;
-					//System.out.println("MOVE");
 
 				}
-		}
+				numMoves++;
+			}
+			if(numMoves > 4 && numMoves <= 8){
+				if(square[random1-1][random2] != '_' && square[random1-1][random2] != '|'){
+					square[random1-1][random2] = this.character;
+					random1 = random1-1;
+				}
+				else if(square[random1+1][random2] != '_' && square[random1+1][random2] != '|'){
+					square[random1+1][random2] = '!';
+					random1 = random1 + 1;
+
+				}
+				else if(square[random1][random2+1] != '_' && square[random1][random2+1] != '|'){
+					square[random1][random2+1] = this.character;
+					random2 = random2+1;
+
+				}
+				else if(square[random1][random2-1] != '_' && square[random1][random2-1] != '|'){
+					square[random1][random2-1] = this.character;
+					random2 = random2-1;
+
+				}
+				numMoves++;
+			}
+
+		}//end else
 		return square;
 	}//end move monster
 
