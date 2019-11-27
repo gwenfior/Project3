7d4a67f86b8e33de545dcad25fe3b0afe61057cf
sburkley <sburkley@mail.umw.edu>
2019-11-27 05:00:09 +0000

    all working pretty much

--- Main.java
+++ Main.java
@@ -28,8 +28,8 @@ public class Main {
 			String restoreFile = input.nextLine();
 			try{
 				Scanner s = new Scanner(new FileReader(restoreFile));
-				Room realRoom = new Room(s);
-				newRoom = realRoom; 
+		//		Room realRoom = new Room(s);
+		//		newRoom = realRoom; 
 				Player player2 = new Player(s);
 				player = player2;
 				
@@ -106,7 +106,7 @@ public class Main {
 						String fileName = input.nextLine();
 						try{	
 							PrintWriter p = new PrintWriter(fileName);
-							newRoom.persist(p);
+						//	newRoom.persist(p);
 							player.persist(p);
 							p.close();        					
 						}catch(Exception e){
