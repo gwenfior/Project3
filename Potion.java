377b1e7c9c50160ae31cf807c01b2408a77bef00
Gwen Fiorillo <gwenfior17@gmail.com>
2019-12-05 23:20:06 +0000

    Documentation of Potion.

--- Potion.java
+++ Potion.java
@@ -1,14 +1,30 @@
 import java.util.Random;
 import java.util.ArrayList;
-public class Potion extends Item{
-	
-	//a usable potion class 
 
+/**
+This class is a subclass of Item that makes a health potion for the Player to use.
+@author Dungeon Crawlers
+  */
+public class Potion extends Item{
+
+	/**
+	Constructor for Potion.
+	@param itemType enum of item type
+	@param name String
+	@param weight int
+	@param value int
+	@param strength int
+	  */
 	public Potion(ItemType itemType, String name, int weight, int value, int strength){	
 		super(ItemType.OTHER, name, weight, value,new Random().nextInt(15)+5);
 
 	}
 
+	/**
+	Overridden method for using a Potion.
+	@param player Player Object
+	@param index int
+	  */
 	public void use(Player player, int index){
 		//add the value of the strength of the potion to the players health
 
