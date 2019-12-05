00d57255f4ef58af1377724f83817bfc00a3e2a2
Gwen Fiorillo <gwenfior17@gmail.com>
2019-12-05 15:30:53 +0000

    Documentation of book, inventory, and character.

--- Book.java
+++ Book.java
@@ -1,16 +1,34 @@
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+/**
+Provides a book object that can be used, and extends from Item.
+@author Dungeon Crawlers
+  */
 public class Book extends Item{
 
 	//usable book class that will generate books that the "use" method will override	
 	private String message;
-
+	
+	/**
+	Creates a book item.
+	@param itemType Other item type
+	@param name Name of book
+	@param weight Weight of book
+	@param value Value of book
+	@param strength strength of book
+	@param message String containing book message
+	  */
 	public Book(ItemType itemType, String name, int weight, int value, int strength, String message){
 		super(ItemType.OTHER, name, weight, value, strength);
 		this.message = message;
 	}
 
+	/**
+	Hydration method for a book.
+	@param s Scanner object
+	@throws NoMoreItemsException if there are no more items
+	  */
 	public Book(Scanner s)throws NoMoreItemsException{
 		this.name = "Book";
 		String category = s.nextLine();
@@ -34,6 +52,10 @@ public class Book extends Item{
 		thing = s.nextLine();	
 	}
 
+	/**
+	Perstistance method for a book.
+	@param pw PrintWriter object
+	  */
 	public void persist(PrintWriter pw){
 		pw.println("Book");
 		pw.println("ItemType.OTHER");
@@ -47,10 +69,19 @@ public class Book extends Item{
 	
 	}
 
+	/**
+	Method that returns a book's message.
+	@return a book's message as a string
+	  */
 	public String getMessage(){
 	return this.message;
 	}
 
+	/**
+	Use method overridden from Item.
+	@param player a player object
+	@param index index of book in inventory
+	  */
 	public void use(Player player, int index){
 		String words = player.getInventory().getItem(index).getMessage();
 	}
--- Character.java
+++ Character.java
@@ -1,24 +1,40 @@
 import java.util.Scanner;
 import java.io.PrintWriter;
 
+/**
+This class is the parent class for player and monster, creates a base character to move and interact in the game.
+@author Dungeon Crawlers
+  */
 public class Character{
 
 	//parent class of monster and player
 	protected String name;
 	protected int health;
 
+	/**
+	Creates a character object with a name and health.
+	@param name Name of Character
+	@param health Health of character
+	  */
 	public Character (String name, int health){
 		this.name = name;
 		this.health = health;
 
 	}
 
+	/**
+	Default constructor for character.
+	  */
 	public Character(){
 		this.name = "default person";
 		this.health = 100;
 
 	}
 
+	/**
+	Hydration method for character.
+	@param s Scanner object
+	  */
 	public Character(Scanner s){
 
 		this.name = s.nextLine();
@@ -26,23 +42,42 @@ public class Character{
 
 	}
 
+	/**
+	Persistance method for Character.
+	@param pw PrintWriter object
+	  */
 	public void persist(PrintWriter pw){
 		pw.println(name);
 		pw.println(health);
 	}
 
+	/**
+	Method to get a character's name.
+	@return name of character
+	  */
 	public String getName(){
 		return this.name;
 	}
 
+	/**
+	Method to get character's health.
+	@return character's health stat
+	  */
 	public int getHealth(){
 		return this.health;
 	}
 
+	/**
+	Method to set health of a character.
+	@param num amount of health points
+	  */
 	public void setHealth(int num){
 		this.health = num;
 	}
 
+	/**
+	Method that returns stats of a character.
+	  */
 	public void stats(){
 		System.out.println("This " + this.name + " has a health of " + this.health + "."); 
 	}
