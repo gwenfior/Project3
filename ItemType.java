aa2d356a8dd75d750ba97e020853ba4a562689ba
Gwen Fiorillo <gwenfior17@gmail.com>
2019-12-05 22:48:13 +0000

    Documentation of ItemType.

--- ItemType.java
+++ ItemType.java
@@ -1,4 +1,7 @@
-//A class that holds the ItemType Enum, Weapoms, armors, or other
+/**
+A class that holds the ItemType Enum, Weapoms, armors, or other.
+@author Dungeon Crawlers
+  */
 enum ItemType{
 
 	WEAPON, ARMOR, OTHER;
